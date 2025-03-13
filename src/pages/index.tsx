a75f924d848ffd81dbf8c26664775c532ecf9081
ZhangWen <zhangwen1932@hotmail.com>
2025-03-14 00:03:55 +0800

    删除不必要代码

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import yayJpg from '../assets/yay.jpg';
-
 export default function HomePage() {
   return (
     <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+     我是一个修丽可电商项目
     </div>
   );
 }
